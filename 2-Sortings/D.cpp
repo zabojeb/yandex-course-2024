82423928f3c9b5d830d3dfc9f5b38289f2b0db7c
zabojeb <zabojeb@zabojeb-2.local>
2024-11-08 14:26:23 +0300

    Add 2.D solution

--- 2-Sortings/D.cpp
+++ 2-Sortings/D.cpp
@@ -1,25 +1,31 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
 #include <vector>
 
-
 using namespace std;
+
+bool compare(const pair<int, int> &a, const pair<int, int> &b) {
+  if (a.first == b.first)
+    return a.second < b.second;
+  return a.first > b.first;
+}
 
 int main() {
   int n;
   cin >> n;
 
-  map<int, int> dict;
-
-  for (int i = 0; i < n; i++) {
-    int x, y;
-    cin >> x >> y;
-
-    dict[x] = y;
+  vector<pair<int, int>> participants(n);
+  for (int i = 0; i < n; ++i) {
+    int id, score;
+    cin >> id >> score;
+    participants[i] = make_pair(score, id);
   }
 
-  // first i need to sort it by keys, than by values
+  sort(participants.begin(), participants.end(), compare);
+
+  for (const auto &p : participants) {
+    cout << p.second << " " << p.first << "\n";
+  }
 
   return 0;
 }
